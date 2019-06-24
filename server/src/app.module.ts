ca3352dfe2c86a3668085535deb5cde0f6b03840
MichaelBusby <mike-volunteer@getmindright.org>
2019-06-23 21:09:48 -0400

    get gql working

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    debug: false,
-    playground: false,
-    autoSchemaFile: 'schema.gql',
-  })],
+  imports: [
+    RecipesModule,
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: 'schema.gql',
+      // context: ({ req }) => ({ req }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
