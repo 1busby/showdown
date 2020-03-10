631d4fe3b6dae4e7e35dbca007d9141e46e8ad0a
Michael Busby <busbyBAM@gmail.com>
2020-03-09 21:09:16 -0400

    run jest

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,35 +1,22 @@
+/* tslint:disable:no-unused-variable */
+
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from 'src/core/core.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [CoreModule],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+    });
+  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  it('should create the app', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'brackets-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('brackets-client');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to brackets-client!');
-  });
+  }));
 });
--- client/src/core/core.module.ts
+++ client/src/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './authentication/auth.module';
 
 @NgModule({
   declarations: [],
-  exports: [AppRoutingModule, AuthModule]
+  exports: [AppRoutingModule, AuthModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
 })
 export class CoreModule {}
