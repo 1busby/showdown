027c002d54196883a5ee0b7f75786e03b6e28ee5
Michael Busby <busbyBAM@gmail.com>
2020-05-10 15:40:32 -0400

    wip

--- server/src/middleware/frontend.middleware.ts
+++ server/src/middleware/frontend.middleware.ts
@@ -19,11 +19,11 @@ const resolvePath = (file: string) => path.resolve(`public/brackets-client${file
 export class FrontendMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const { originalUrl } = req;
-    console.log('LOOK >>> incoming request url = ' + originalUrl);
+    console.log('FrontendMiddleware - originalUrl', JSON.stringify(originalUrl));
     if (originalUrl.indexOf('graphql') === 1) {
       // it starts with /api --> continue with execution
       next();
-    } else if (allowedExt.filter(ext => originalUrl.includes(ext) > 0).length > 0) {
+    } else if (allowedExt.filter(ext => originalUrl.includes(ext)).length > 0) {
       // it has a file extension --> resolve the file
       res.sendFile(resolvePath(originalUrl));
     } else {
