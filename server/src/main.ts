7486108479ecc3593ee44d5603907f30e3534443
Michael Busby <mbusbyIT@gmail.com>
2020-05-22 13:47:56 -0400

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const whitelist = [
+  'http://localhost:4200',
+]
+const corsOptions = {
+  origin(origin, callback) {
+    console.log('CORS callback - Origin: ' + origin);
+
+    return callback(null, true);
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'));
+    }
+  },
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
+  app.enableCors(corsOptions);
   await app.listen(process.env.PORT);
 }
 bootstrap();
