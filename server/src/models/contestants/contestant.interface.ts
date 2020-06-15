22477815c25265d67effc27978144b790bb10c60
Michael Busby <mbusbyIT@gmail.com>
2020-06-14 23:42:33 -0400

    wip

--- server/src/models/contestants/contestant.interface.ts
+++ server/src/models/contestants/contestant.interface.ts
@@ -1,14 +1,10 @@
 import { InterfaceType, Field, ID, Int } from '@nestjs/graphql';
 import { IContestant } from '../../../../shared/models';
 import { User } from '../users/user';
-import { AnonymousUser } from '../users/anonymousContestant';
 
 @InterfaceType({
   resolveType(contestant) {
-    // if (contestant.isRegistered) {
-    // }
     return User;
-    return AnonymousUser;
   },
 })
 export abstract class Contestant implements Partial<IContestant> {
