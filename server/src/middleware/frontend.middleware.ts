9cfbd82889fb8d0f114ae324120396c2c855ae42
Michael Busby <mbusbyIT@gmail.com>
2020-06-16 03:06:42 -0400

    wip

--- server/src/middleware/frontend.middleware.ts
+++ server/src/middleware/frontend.middleware.ts
@@ -22,8 +22,10 @@ export class FrontendMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const { originalUrl } = req;
     if (originalUrl.indexOf('graphql') === 1) {
-      // it starts with /api --> continue with execution
+      // it starts with /graphql --> continue with execution
       next();
+    } else if (originalUrl.indexOf('health') === 1) {
+      res.sendFile(resolvePath('/health.html'));
     } else if (allowedExt.filter(ext => originalUrl.includes(ext)).length > 0) {
       // it has a file extension --> resolve the file
       res.sendFile(resolvePath(originalUrl));
