28c1d06e5372b6d22b0ac525cb57b8c1d922ca04
Michael Busby <busbyBAM@gmail.com>
2020-07-11 21:05:27 -0400

    wip

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,13 +4,6 @@ module.exports = {
       name: 'brackets',
       script: 'dist/main.js',
       node_args: '-r ./tsconfig-paths-bootstrap.js',
-      exec_mode: 'cluster',
-      env: {
-        DATABASE_URL:
-          'mongodb+srv://alex:keznH2XVg9efQtu4@cluster0-cez0a.mongodb.net/brackets?retryWrites=true&w=majority',
-        PORT: 3000,
-        ISPRODUCTION: false,
-      },
       env_production: {
         DATABASE_URL:
           'mongodb+srv://alex:keznH2XVg9efQtu4@cluster0-cez0a.mongodb.net/brackets?retryWrites=true&w=majority',
@@ -27,11 +20,10 @@ module.exports = {
       host: '54.204.160.247',
       ref: 'origin/master',
       repo: 'git@github.com:mbusbyHub/brackets.git',
-      path: '/brackets',
+      path: '~/brackets',
       'pre-deploy-local': '',
       'post-deploy':
         'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
     },
   },
 };
