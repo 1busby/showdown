9203bd91de849c3a62167f21e8f7c819e9ebb2d0
Michael Busby <busbyBAM@gmail.com>
2020-07-11 23:36:25 -0400

    more health

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,20 +4,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Models
+import { HealthController } from 'health.controller';
 import { TournamentsModule } from './models/tournament/tournament.module';
 import { UsersModule } from './models/user/user.module';
 import { MatchModule } from './models/match/match.module';
-import { join } from 'path';
 
 @Module({
   imports: [
     TerminusModule,
+    HealthController,
     ConfigModule.forRoot({
       load: [configuration],
     }),
